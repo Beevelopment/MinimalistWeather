b3e905ad170984979c630467b769c2760a10848e
Carl Henningsson <Henningsson@MacBook-Pro.lan>
2018-12-27 14:20:02 +0100

    Location search function is now functioning

---
+++ minimalistweather/View/SearchLocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchLocationCell.swift
+//  minimalistweather
+//
+//  Created by Carl Henningsson on 12/27/18.
+//  Copyright © 2018 Carl Henningsson. All rights reserved.
+//
+
+import UIKit
+
+class SearchLocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
